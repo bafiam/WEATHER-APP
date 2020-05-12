c9c89a20451a6ee20938ef1614b747b05d3b4bff
bafiam <stevebafiam@gmail.com>
2020-05-12 10:10:50 +0300

    feature:add fileloader, cssloader and setup webpack config commands

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss|css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
